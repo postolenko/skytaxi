5f9c717d8a32bd594d06c567726b962bc9f4c95d
postolenko <denis.postolenko@gmail.com>
2017-10-15 01:19:24 +0300

    Sky Taxi

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+
+    var marginTop;
+
+    // ----------------------------
+
+    var setFooterPositionInterval;
+    var contentCoor;
+    var footerCoor;
+
+    // ----------------------------    
+
+    getFooterPosition();
+
+    getCenterPosition();
+
+    $(window).resize(function() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+        // ----------------------
+
+        getCenterPosition();
+
+    });
+
+
+    $(function() {
+
+
+
+    });
+
+
+    function getCenterPosition() {
+
+        paddingTop = ( $(".wrapper").height() - $(".center").height() - $(".footer").height() ) / 2;
+
+        $(".wrapper").css({
+            paddingTop : paddingTop + "px"
+        });
+
+    }
+
+
+    function getFooterPosition() {
+
+        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+        setFooterPositionInterval = setInterval(function() {
+
+            contentCoor = $(".content").offset().top + $(".content").height();
+            footerCoor = $(".footer").offset().top;
+
+            if( contentCoor != footerCoor ) {
+
+                $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+                $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+                clearInterval(setFooterPositionInterval);
+
+            }
+
+        }, 35);
+
+    }
+
+
+
+});
